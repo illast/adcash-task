f09926bfeb9df31c070349eb6cd2dfadfe2595da
lastovkoi <lastovkoi@gmail.com>
2024-02-26 16:27:23 +0200

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+from app import routes
+
+with app.app_context():
+    db.create_all()
