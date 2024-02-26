6c0dcd6d04be83bb854573ba8e1663074ec2a25c
lastovkoi <lastovkoi@gmail.com>
2024-02-26 16:57:44 +0200

    Added route to money transfer

--- app/routes.py
+++ app/routes.py
@@ -45,6 +45,39 @@ def get_balance():
     return jsonify({'btc_balance': btc_balance, 'eur_balance': eur_balance})
 
 
+@app.route('/transfer', methods=['POST'])
+def transfer():
+    req_data = request.get_json()
+    amount_eur = req_data.get('amount_eur')
+
+    if not amount_eur:
+        return jsonify({'message': 'Missing amount_eur parameter'}), 400
+
+    exchange_rate = get_exchange_rate()
+    if not exchange_rate:
+        return jsonify({'message': 'Failed to retrieve exchange rate'}), 500
+    amount_btc = amount_eur / exchange_rate
+
+    unspent_transactions = Transaction.query.filter_by(spent=False).all()
+    btc_balance = sum(transaction.amount for transaction in unspent_transactions)
+
+    if amount_btc > btc_balance or amount_btc < 0.00001:
+        return jsonify({'message': 'Invalid transfer amount or insufficient balance'}), 400
+
+    for transaction in unspent_transactions:
+        transaction.spent = True
+        amount_btc -= transaction.amount
+        if amount_btc <= 0:
+            break
+
+    if amount_btc < 0:
+        new_transaction = Transaction(abs(amount_btc))
+        db.session.add(new_transaction)
+
+    db.session.commit()
+    return jsonify({'message': 'Transfer successful'}), 200
+
+
 def get_exchange_rate():
     response = requests.get('http://api-cryptopia.adca.sh/v1/prices/ticker')
     if response.status_code == 200:
