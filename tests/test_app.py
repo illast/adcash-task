0538240e11d4f43ff169e37b886ea306cb68578b
lastovkoi <lastovkoi@gmail.com>
2024-02-26 17:56:05 +0200

    Added more tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -34,7 +34,7 @@ def test_list_transactions(client):
 
 def test_get_balance(client, monkeypatch):
     def mock_get_exchange_rate():
-        return 50000
+        return 40000
 
     monkeypatch.setattr('app.routes.get_exchange_rate', mock_get_exchange_rate)
 
@@ -48,7 +48,7 @@ def test_get_balance(client, monkeypatch):
     assert data['eur_balance'] == (5.5 + 10) * mock_get_exchange_rate()
 
 
-def test_transfer(client, monkeypatch):
+def test_transfer_successful_created_new_transaction(client, monkeypatch):
     def mock_get_exchange_rate():
         return 40000
 
@@ -73,3 +73,86 @@ def test_transfer(client, monkeypatch):
             assert item['spent']
         if item['amount'] == 0.5:
             assert not item['spent']
+
+
+def test_transfer_successful_no_new_transaction_created(client, monkeypatch):
+    def mock_get_exchange_rate():
+        return 40000
+
+    monkeypatch.setattr('app.routes.get_exchange_rate', mock_get_exchange_rate)
+
+    client.post('/transactions/add', json={'amount': 3})
+    client.post('/transactions/add', json={'amount': 2})
+
+    response = client.post('/transfer', json={'amount_eur': 5 * mock_get_exchange_rate()})
+    assert response.status_code == 200
+    assert b'Transfer successful' in response.data
+
+    response = client.get('/transactions')
+    data = response.json
+    assert len(data['transactions']) == 2
+
+    for item in data['transactions']:
+        assert item['spent']
+
+
+def test_transfer_not_successful_no_necessary_parameters(client, monkeypatch):
+    def mock_get_exchange_rate():
+        return 40000
+
+    monkeypatch.setattr('app.routes.get_exchange_rate', mock_get_exchange_rate)
+
+    client.post('/transactions/add', json={'amount': 3})
+    client.post('/transactions/add', json={'amount': 2})
+
+    response = client.post('/transfer', json={'amount_btc': 5 * mock_get_exchange_rate()})
+    assert response.status_code == 400
+    assert b'Missing amount_eur parameter' in response.data
+
+    response = client.get('/transactions')
+    data = response.json
+    assert len(data['transactions']) == 2
+
+    for item in data['transactions']:
+        assert not item['spent']
+
+
+def test_transfer_invalid_amount(client, monkeypatch):
+    def mock_get_exchange_rate():
+        return 1
+
+    monkeypatch.setattr('app.routes.get_exchange_rate', mock_get_exchange_rate)
+
+    client.post('/transactions/add', json={'amount': 3})
+    client.post('/transactions/add', json={'amount': 2})
+
+    response = client.post('/transfer', json={'amount_eur': 0.000001})
+    assert response.status_code == 400
+    assert b'Invalid transfer amount or insufficient balance' in response.data
+
+    response = client.get('/transactions')
+    data = response.json
+    assert len(data['transactions']) == 2
+
+    for item in data['transactions']:
+        assert not item['spent']
+
+
+def test_transfer_insufficient_balance(client, monkeypatch):
+    def mock_get_exchange_rate():
+        return 40000
+
+    monkeypatch.setattr('app.routes.get_exchange_rate', mock_get_exchange_rate)
+
+    client.post('/transactions/add', json={'amount': 3})
+
+    response = client.post('/transfer', json={'amount_eur': 4 * mock_get_exchange_rate()})
+    assert response.status_code == 400
+    assert b'Invalid transfer amount or insufficient balance' in response.data
+
+    response = client.get('/transactions')
+    data = response.json
+    assert len(data['transactions']) == 1
+
+    for item in data['transactions']:
+        assert not item['spent']
